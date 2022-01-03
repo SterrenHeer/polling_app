c020624b44532f5d463af816d41bbdc2c4e772c4
SterrenHeer <sterrenheer@gmail.com>
2022-01-03 16:40:59 +0300

    Add poll creating page

--- poll/views.py
+++ poll/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def home(request):
     context = {}
     return render(request, 'poll/home.html', context)
+
+
+def create(request):
+    context = {}
+    return render(request, 'poll/create.html', context)
--- polling_app/urls.py
+++ polling_app/urls.py
@@ -20,4 +20,5 @@ from poll import views as poll_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', poll_views.home, name='home'),
+    path('create/', poll_views.create, name='create'),
 ]
