9c39cc06fd0f444454a2c2d305d1b7dacda10f44
SterrenHeer <sterrenheer@gmail.com>
2022-01-03 16:28:02 +0300

    Add homepage

--- polling_app/urls.py
+++ polling_app/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from poll import views as poll_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', poll_views.home, name='home'),
 ]
